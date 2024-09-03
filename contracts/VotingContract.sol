0592adf4604c8c9324e66d1bb08d679db46e291c
Riya <riyachudasama1123@gmail.com>
2024-09-03 22:20:03 +0530

    smart contract commit

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -1,26 +1,216 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.9;
-// import "hardhat/console.sol";
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/* Counters.sol: This import allows the use of counters that can increment or decrement, typically for keeping track of unique IDs*/
 
 contract VotingContract {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public _voterId; //A counter to keep track of the unique IDs for voters.
+    Counters.Counter public _candidateId;
+
+    /* The Counters.Counter type from OpenZeppelin's Counters library is attached to the Counters data structure. It allows easy use of counters for tracking things like voter IDs and candidate IDs.*/
+
+    //owner's address
+    address public votingOrganizer;
+
     struct Candidate {
-        uint256 id;
+        uint256 candidateId;
+        string age;
         string name;
-        uint256 numberOfVotes;
+        string image;
+        uint256 voteCount;
+        address _address;
+        string ipfs; //all info about candidate
     }
 
-    //List of all candidates
-    Candidate[] public candidates;
-    //owner's address
-    address public owner;
-    //map all voters' address
-    mapping(address => bool) public voters;
-    //list of voters
-    address[] public listOfVoters;
+    event CandidateCreate(
+        uint256 indexed candidateId,
+        string age,
+        string name,
+        string image,
+        uint256 voteCount,
+        address _address,
+        string ipfs
+    );
 
-    //create a voting end and start session
-    uint256 public votingStart;
-    uint256 public votingEnd;
+    address[] public candidateAddress;
 
-    //create an election status
+    mapping(address => Candidate) public candidates;
+
+    /////----End of candidate data
+
+    //voter data
+
+    address[] public votedVoters;
+
+    address[] public votersAddress;
+    mapping(address => Voter) public voters;
+
+    struct Voter {
+        uint256 voter_voterId;
+        string voter_name;
+        string voter_image;
+        address voter_address;
+        uint256 voter_allowed;
+        bool voter_voted;
+        uint256 voter_vote;
+        string voter_ipfs;
+    }
+
+    event VoterCreate(
+        uint256 indexed voter_voterId,
+        string voter_name,
+        string voter_image,
+        address voter_address,
+        uint256 voter_allowed,
+        bool voter_voted,
+        uint256 voter_vote,
+        string voter_ipfs
+    );
+
+    ////---End of Voter data
+
+    constructor() {
+        votingOrganizer = msg.sender;
+    }
+
+    function setCandidate(
+        address _address,
+        string memory _age,
+        string memory _name,
+        string memory _image,
+        string memory _ipfs
+    ) public {
+        require(
+            votingOrganizer == msg.sender,
+            "Only voting organizers can create the candidate"
+        );
+
+        _candidateId.increment();
+
+        uint256 idNumber = _candidateId.current();
+        //give current id number
+
+        Candidate storage candidate = candidates[_address];
+
+        candidate.age = _age; //take from user
+        candidate.name = _name;
+        candidate.candidateId = idNumber;
+        candidate.image = _image;
+        candidate.voteCount = 0;
+        candidate._address = _address;
+        candidate.ipfs = _ipfs;
+
+        candidateAddress.push(_address); //push all candidate's addresses in array
+
+        emit CandidateCreate( //order same as event
+            idNumber,
+            _age,
+            _name,
+            _image,
+            candidate.voteCount,
+            _address,
+            _ipfs
+        );
+    }
+
+    function getCandidate() public view returns (address[] memory) {
+        return candidateAddress;
+    }
+
+    function getCandidateLength() public view returns (uint256) {
+        return candidateAddress.length;
+    }
+
+    function getCandidateData(
+        address _address
+    )
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            uint256,
+            string memory,
+            uint256,
+            string memory,
+            address
+        )
+    {
+        return (
+            candidates[_address].age,
+            candidates[_address].name,
+            candidates[_address].candidateId,
+            candidates[_address].image,
+            candidates[_address].voteCount,
+            candidates[_address].ipfs,
+            candidates[_address]._address
+        );
+    }
+
+    ///voter's section
+
+    function voterRight(
+        address _address,
+        string memory _name,
+        string memory _image,
+        string memory _ipfs
+    ) public {
+        require(
+            votingOrganizer == msg.sender,
+            "Only organizer can create voter"
+        );
+
+        _voterId.increment();
+
+        uint256 idNumber = _voterId.current();
+
+        Voter storage voter = voters[_address];
+
+        require(voter.voter_allowed == 0);
+        //means the voter is not already registered in the contest
+
+        voter.voter_name = _name;
+        voter.voter_image = _image;
+        voter.voter_address = _address;
+        voter.voter_allowed = 1;
+        voter.voter_voted = false;
+        voter.voter_vote = 1000;
+        voter.voter_ipfs = _ipfs;
+
+        votersAddress.push(_address);
+
+        emit VoterCreate(
+            idNumber,
+            _name,
+            _image,
+            _address,
+            voter.voter_allowed,
+            voter.voter_voted,
+            voter.voter_vote,
+            _ipfs
+        );
+    }
+
+    function vote(
+        address _candidateAddress,
+        uint256 _candidateVoteId
+    ) external {
+        Voter storage voter = voters[msg.sender];
+
+        require(!voter.voter_voted, "You have already voted!");
+        require(voter.voter_allowed != 0, "You have no right to vote!");
+
+        voter.voter_voted = true;
+        voter.voter_vote = _candidateVoteId;
+
+        votedVoters.push(msg.sender);
+
+        candidates[_candidateAddress].voteCount += voter.voter_allowed;
+    }
 }
